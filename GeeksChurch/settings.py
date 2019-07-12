de9f4ec16ba112d50f7815352ec2c8b0765ea6b4
asaern9 <asaern9@gmail.com>
2019-07-12 23:33:21 +0000

    Update settings.py

--- GeeksChurch/settings.py
+++ GeeksChurch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*w8e*gt5jp!aq1lu5%o_&t#e62*x#@^g_osg(0w094-pwc#(*1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
